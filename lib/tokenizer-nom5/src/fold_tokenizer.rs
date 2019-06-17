3bc0ff9956bcaababe8c1b4902ebbdb64c47e783
Alexander Reece <alreece45@gmail.com>
2019-06-17 02:18:12 -0400

    Add tokenizer for nom 5.x

---
+++ lib/tokenizer-nom5/src/fold_tokenizer.rs
@@ -0,0 +1,43 @@
+
+use nom::{alt, call, delimited, fold_many0, is_not, named, one_of, preceded, recognize, tag, ws};
+
+use super::Nom5Tokenizer;
+use super::{start_of_sequence, start_of_sequence_without_b};
+
+#[derive(Default)]
+pub struct Nom5FoldTokenizer {}
+
+named!(string_contents<&[u8]>,
+    alt!(tag!("\\\"") | tag!("\\") | is_not!("\\\""))
+);
+
+impl Nom5Tokenizer for Nom5FoldTokenizer {
+    named!(token<&[u8]>, ws!(
+        alt!(
+            call!(start_of_sequence)
+            | recognize!(
+                delimited!(
+                    alt!(tag!("\"") | tag!("b\"")),
+                    fold_many0!(
+                        call!(string_contents),
+                        0u8, |acc, _| -> u8 { acc }
+                    ),
+                    tag!("\"")
+                )
+            )
+            | recognize!( preceded!(tag!("b"), call!(start_of_sequence_without_b)) )
+            | tag!(b"==") | tag!(b"!=") | tag!(b"&&")
+            | tag!(b"||") | tag!(b"**") | tag!(b"..")
+            | recognize!(one_of!("()[],.|&^!+-*/%"))
+        )
+    ));
+}
+
+#[cfg(test)]
+mod tests {
+    use tokenizer_common::test_tokenizer;
+
+    use super::super::Nom5VecTokenIterator;
+
+    test_tokenizer!(Nom5VecTokenIterator::from_slice);
+}
