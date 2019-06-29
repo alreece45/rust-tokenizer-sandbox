36ddee02118cf4c30ee249a324589f1dbc0b521c
Alexander Reece <alreece45@gmail.com>
2019-06-29 15:07:22 -0400

    Add is_token() function to tokenizer_common

--- lib/tokenizer-common/src/lib.rs
+++ lib/tokenizer-common/src/lib.rs
@@ -21,6 +21,19 @@ pub fn is_start_of_two(c: u8) -> bool {
 }
 
 #[inline]
+pub fn is_token(slice: &[u8]) -> bool {
+    match slice.len() {
+        1 => is_start_prefix(slice[0]),
+        2 => match slice {
+            b".." | b"**" | b"==" | b"!=" |
+            b"&&" | b"||" | b"b\"" => true,
+            _ => false,
+        }
+        _ => false,
+    }
+}
+
+#[inline]
 pub fn is_start_prefix(c: u8) -> bool {
     #[cfg_attr(rustfmt, rustfmt_skip)] 
     match c {
